f85c38b88d5c2cde0da5dec4f857740f9f785be8
aravindhsamidurai@gmail.com <Rithish@131323>
2021-09-13 22:05:28 +0530

    Initial Commit

---
+++ app/src/main/java/com/aravindh/androidcore/CustomAlertDialog.kt
@@ -0,0 +1,22 @@
+package com.aravindh.androidcore
+
+import android.content.Context
+import androidx.appcompat.app.AlertDialog
+import java.lang.NullPointerException
+
+object CustomAlertDialog {
+
+    fun showCustomDialog(context: Context?, cancelable: Boolean = true) {
+        if (context == null) {
+            throw NullPointerException("Context should be not null")
+        }
+
+        AlertDialog.Builder(context)
+            .setTitle("This is my Title")
+            .setMessage("This is my sample messge")
+            .setCancelable(cancelable)
+            .show()
+    }
+
+}
+
