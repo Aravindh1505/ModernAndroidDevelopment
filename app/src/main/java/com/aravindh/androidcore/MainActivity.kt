2310a2864b40c8c4094667b60c309f228c0c84de
Aravindh1505 <aravindh.samidurai@gmail.com>
2022-08-18 23:10:14 +0530

    Jetpack Compose Base Setup

--- app/src/main/java/com/aravindh/androidcore/MainActivity.kt
+++ app/src/main/java/com/aravindh/androidcore/MainActivity.kt
@@ -1,15 +1,59 @@
 package com.aravindh.androidcore
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import dagger.hilt.android.AndroidEntryPoint
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
-@AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(color = Color.Yellow)
+            .padding(5.dp),
+        horizontalAlignment = Alignment.Start,
+    ) {
+        CustomText(text = "Hello $name!")
+        CustomText(text = "Hello $name!")
+        CustomText(text = "Hello $name!")
+        CustomText(text = "Hello $name!")
+        CustomText(text = "Hello $name!")
+        CustomText(text = "Hello $name!")
 
     }
+
+}
+
+@Composable
+fun CustomText(text: String) {
+    Text(
+        text = "Hello $text!",
+        modifier = Modifier
+            .padding(5.dp)
+            .clickable {
+                println("Clicked!")
+            }
+    )
 }
