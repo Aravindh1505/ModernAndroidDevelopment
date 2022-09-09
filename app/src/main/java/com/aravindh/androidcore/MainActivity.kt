9704f8a1b04ada2a47aa233868e141e41c0886bb
asamidur-ford <asamidur@ford.com>
2022-09-09 11:59:09 +0530

    Base setup for upcoming concepts

--- app/src/main/java/com/aravindh/androidcore/MainActivity.kt
+++ app/src/main/java/com/aravindh/androidcore/MainActivity.kt
@@ -1,15 +1,7 @@
 package com.aravindh.androidcore
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.*
-
-fun logger(message: String) {
-    Log.d(TAG, message)
-}
-
-const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,41 +9,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val handler = CoroutineExceptionHandler{ _ , throwable ->
-            logger("Exception caughted ${throwable.message}")
-        }
-
-        CoroutineScope(Dispatchers.IO + handler).launch {
-            supervisorScope {
-
-                launch {
-                    delay(1000L)
-                    throw Exception("First coroutine error")
-                }
-
-                launch {
-                    delay(1000L)
-                    logger("second coroutine success")
-                }
-
-                launch {
-                    delay(1000L)
-                    throw Exception("Third coroutine error")
-                }
-            }
-        }
-
     }
-
-    suspend fun networkCall1(): String {
-        delay(8000L)
-        return "This is answer for network call 1"
-    }
-
-    suspend fun networkCall2(): String {
-        delay(2000L)
-        return "This is answer for network call 2"
-    }
-
 }
--- app/src/main/java/com/aravindh/androidcore/References.kt
+++
@@ -1,3 +0,0 @@
-package com.aravindh.androidcore
-
-// https://www.youtube.com/watch?v=ShNhJ3wMpvQ&list=PLQkwcJG4YTCQcFEPuYGuv54nYai_lwil_&index=1
