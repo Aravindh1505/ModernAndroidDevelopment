885a8fda46b978d240a3f10b7cddb0b1b8dba3b8
asamidur-ford <asamidur@ford.com>
2022-09-27 18:24:37 +0530

    Compose Meal App Setup

--- app/src/main/java/com/aravindh/androidcore/MainActivity.kt
+++ app/src/main/java/com/aravindh/androidcore/MainActivity.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -31,7 +33,7 @@ fun Greeting(name: String) {
         Modifier
             .fillMaxWidth()
             .wrapContentHeight()
-            .background(color = Color.Yellow)
+            .background(color = Color.White)
             .padding(5.dp),
         horizontalAlignment = Alignment.Start,
     ) {
@@ -49,11 +51,28 @@ fun Greeting(name: String) {
 @Composable
 fun CustomText(text: String) {
     Text(
-        text = "Hello $text!",
+        text = "$text!",
         modifier = Modifier
             .padding(5.dp)
             .clickable {
                 println("Clicked!")
             }
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultUI() {
+    Greeting("Android")
+//    CustomText(text = "Hello !")
+//    CustomButton()
+}
+
+@Composable
+fun CustomButton() {
+    Button(onClick = {
+        println("Clicked..")
+    }) {
+        CustomText(text = "Hello!")
+    }
 }
