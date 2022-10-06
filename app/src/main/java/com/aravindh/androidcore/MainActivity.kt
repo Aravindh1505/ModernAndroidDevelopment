1f0dbde1e8848b5c8a50daefb0ec7d58ae1a77d7
asamidur-ford <asamidur@ford.com>
2022-10-06 19:50:37 +0530

    Compose state management

--- app/src/main/java/com/aravindh/androidcore/MainActivity.kt
+++ app/src/main/java/com/aravindh/androidcore/MainActivity.kt
@@ -3,21 +3,21 @@ package com.aravindh.androidcore
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.aravindh.androidcore.ui.theme.MyApplicationTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,39 +34,37 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen() {
+    val nameList = remember { mutableStateListOf("Aravindh", "Aadhira") }
+
+    val name = remember { mutableStateOf("") }
+
     Surface(
         color = Color.White,
         modifier = Modifier.fillMaxSize()
     ) {
         Column(
-            verticalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxHeight(),
+            verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Row(
-                horizontalArrangement = Arrangement.SpaceAround,
-                verticalAlignment = Alignment.Top,
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                HorizontalBar(Color.Red)
-                HorizontalBar(Color.Green)
-            }
-            HorizontalBar(Color.Yellow)
-            HorizontalBar(Color.Black)
-            HorizontalBar(Color.Blue)
+            GreetingList(nameList, { nameList.add(name.value) }, name)
         }
     }
 }
 
 @Composable
-private fun HorizontalBar(color: Color) {
-    Surface(
-        color = color,
-        modifier = Modifier
-            .height(100.dp)
-            .width(100.dp)
-    ) {
-
+private fun GreetingList(
+    nameList: List<String>,
+    buttonClick: () -> Unit,
+    name: MutableState<String>
+) {
+    for (names in nameList) {
+        Text(text = names)
     }
+
+    BasicTextField(value = name.value, onValueChange = { name.value = it })
+
+    Button(onClick = buttonClick) { Text(text = "Add Name") }
 }
 
 
